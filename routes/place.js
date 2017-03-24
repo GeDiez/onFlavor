d369b59b13034372783b525ca919e6b2beeee397
AngelGallegos <7angelgallegos@gmail.com>
2017-03-24 17:06:03 -0600

    initial databases migrations

---
+++ routes/place.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = express.Router();
+const PlacesService = require('../services/places_service');
+/* GET home page. */
+router.get('/', (req, res, next) => {
+  PlacesService.fetch().then((places)=> {
+    res.json(places);
+  });
+});
+
+router.get('/:id', (req, res, next) => {
+  PlacesService.getById(req.params.id).then((response) => {
+    res.json(response);
+  })
+});
+
+router.post('/', (req, res, next) =>{
+  const place = {
+    name: req.body.name,
+    latitude: req.body.latitude,
+    longitude: req.body.longitude
+  };
+  PlacesService.createOrUpdateWithObj(place).then((message) => {
+    res.json(message);
+  });
+});
+
+router.put('/:id', (req, res, next) => {
+  const place = {
+    name: req.body.name,
+    latitude: req.body.latitude,
+    longitude: req.body.longitude,
+    id: req.params.id
+  };
+  PlacesService.createOrUpdateWithObj(place).then((newPlace) => {
+    res.json(newPlace)
+  }).catch((error) => {
+    res.status(500).json(error);
+  })
+});
+
+module.exports = router;
--- services/places_service.js
+++ services/places_service.js
@@ -9,6 +9,35 @@ module.exports = {
         resolve(places.toJSON());
       });
     });
+  },
+
+  createOrUpdateWithObj: (place) => {
+    return new Promise((resolve, reject) => {
+      if (!place.id) {
+        new Place().save(place).then((place) => {
+          resolve(place);
+        });
+      } else {
+        Place.where('id',place.id).fetch().then((localPlace) => {
+          if(localPlace){
+            localPlace.save(place).then((newPlace) => {
+              resolve(newPlace);
+            });
+          }
+          else {
+            reject({error: 'no existe pendeji'})
+          }
+        }).catch((error) => {reject(error)});
+      }
+    });
+  },
+
+  getById: (id) => {
+    return new Promise((resolve, reject) => {
+      Place.where('id', id).fetch().then(place => {
+        resolve(place);
+      });
+    })
   }
 
 }
