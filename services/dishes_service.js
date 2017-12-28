838c023ae3b2547b3e8a5948d3a7d27beb7a8d6e
Gibran Lopez Morales <gibran@michelada.io>
2017-12-28 13:16:34 -0600

    generate bundlejs

--- services/dishes_service.js
+++ services/dishes_service.js
@@ -1,62 +1,66 @@
 const bookshelf = require('../bookshelf');
 const Dish = require('../models/Dish');
-const knex = bookshelf.knex;
 
 module.exports = {
   fetch: () => {
-    return new Promise((resolve, reject)=>{
-      Dish.fetchAll().then((dishes)=>{
+    return new Promise((resolve, reject) => {
+      Dish.fetchAll().then(dishes => {
         resolve(dishes.toJSON());
       });
     });
   },
 
-  createOrUpdateWithObj: (dish) => {
+  createOrUpdateWithObj: dish => {
     return new Promise((resolve, reject) => {
       if (!dish.id) {
-        new Dish().save(dish).then((dish) => {
-          if(dish){
+        new Dish().save(dish).then(dish => {
+          if (dish) {
             resolve(dish);
-          }
-          else {
-            resolve({error: "Dish was not created"});
+          } else {
+            resolve({ error: 'Dish was not created' });
           }
         });
       } else {
-        Dish.where('id', dish.id).fetch().then((localDish) => {
-          if(localDish){
-            localDish.save(dish).then((newDish) => {
-              resolve(newDish);
-            });
-          }
-          else {
-            reject({error: 'Dish not found'});
-          }
-        }).catch((error) => {resolve(error)});
+        Dish.where('id', dish.id)
+          .fetch()
+          .then(localDish => {
+            if (localDish) {
+              localDish.save(dish).then(newDish => {
+                resolve(newDish);
+              });
+            } else {
+              reject({ error: 'Dish not found' });
+            }
+          })
+          .catch(error => {
+            resolve(error);
+          });
       }
     });
   },
 
-  getById: (id) => {
+  getById: id => {
     return new Promise((resolve, reject) => {
-      Dish.where('id', id).fetch().then(dish => {
-        resolve(dish);
-      });
-    })
+      Dish.where('id', id)
+        .fetch()
+        .then(dish => {
+          resolve(dish);
+        });
+    });
   },
 
-  deleteById: (dishId) => {
+  deleteById: dishId => {
     return new Promise((resolve, reject) => {
-      Dish.where('id', dishId).fetch().then(dish => {
-        if(dish){
-          dish.destroy();
-          resolve({message: 'Dish deleted'});
-        }
-        else {
-          reject({error: 'Dish not found'});
-        }
-      })
-    })
-  }
-
-}
+      Dish.where('id', dishId)
+        .fetch()
+        .then(dish => {
+          if (dish) {
+            dish.destroy();
+            resolve({ message: 'Dish deleted' });
+          } else {
+            reject({ error: 'Dish not found' });
+          }
+        });
+    });
+  },
+};
